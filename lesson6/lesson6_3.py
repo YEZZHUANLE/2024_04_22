93336b67c162d860c2bc2109ac4e7375ac64b492
YEZZHUANLE <167630357+YEZZHUANLE@users.noreply.github.com>
2024-05-06 16:20:15 +0000

    modify

---
+++ lesson6/lesson6_3.py
@@ -0,0 +1,24 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import pyinputplus as pyip\n",
+    "\n",
+    "score = pyip.inputInt(\"請輸入學生分數(最高300分)\",min=0,max=300)\n",
+    "\n",
+    "print(score)"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
